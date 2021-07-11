d90a91d1c9ccaac7fe8ddd18e1f4b278d49b0b1b
Jason Rosenzweig <mail4jasonr@gmail.com>
2021-07-11 16:51:30 +0800

    Switched partitions from mbr to gpt whoops

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ check() {
 partition_disk() {
     echo "Partitioning disk"
     sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk ${disk} >/dev/null 2>>error.txt || error=true
-    o # clear the in memory partition table
+    g # clear the in memory partition table
     n # new partition
     p # primary partition
     1 # partition number 1
